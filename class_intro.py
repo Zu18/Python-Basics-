9551eb7a0302495657862a8350bad1bbe25bcb98
Zu18 <zuhra.galieva.2014@gmail.com>
2022-11-04 08:01:11 +0000

    Add the file 'class_intro.py'

---
+++ class_intro.py
@@ -0,0 +1,12 @@
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "pink"
+
+violet = Flower()
+violet.color = "blue"
+
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
