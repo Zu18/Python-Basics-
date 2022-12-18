76a550f5b2d27017bb90e4471bcb49208f3ed55a
Zukhra <zuhra.galieva.2014@gmail.com>
2022-12-18 11:55:12 +0000

    Add the file 'cubes.py'

---
+++ cubes.py
@@ -0,0 +1,15 @@
+'''
+number of cuts = x
+The total number of cubes = (x+1)^3
+the number of all blue cubes = (x-1)^3
+
+Number of cubes with one or more red squares:
+= (x+1)^3 - (x-1)^3
+= (x+1)(x+1)(x+1) - (x-1)(x-1)(x-1)
+= x^3 + 3x^2 + 3x + 1 - (x^3 - 3x^2 + 3x -1 )
+= 6x^2 + 2
+
+'''
+
+def count_squares(x):
+    return 6 * x**2 + 2
